217257bb352f04da59cfba7c9055a5282f45b7cc
Alexis Lecanu <alexis.lecanu@gmail.com>
2014-03-13 11:41:10 +0100

    fixed bug destroy

--- popupsearch/src/main/java/fr/ravenfeld/librairies/popupsearch/example/HomeFragment.java
+++ popupsearch/src/main/java/fr/ravenfeld/librairies/popupsearch/example/HomeFragment.java
@@ -141,8 +141,6 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.e("TEST", "onCreateView home");
-
         View rootView = inflater.inflate(R.layout.fragment_home, null);
 
         return rootView;
@@ -153,7 +151,9 @@ public class HomeFragment extends Fragment {
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mActionPopupSearch.dismiss();
+        if(mActionPopupSearch!=null) {
+            mActionPopupSearch.dismiss();
+        }
     }
 
     private class DataRetriever extends AsyncTask<Void, Void, Boolean> {
